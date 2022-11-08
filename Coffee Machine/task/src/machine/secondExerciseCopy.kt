b1a5323ff0685a9a108e93b1b837ac479f27d178
borkusgod <lightraven.bourque@gmail.com>
2022-11-08 10:05:02 -0400

    Initial commit

---
+++ Coffee Machine/task/src/machine/secondExerciseCopy.kt
@@ -0,0 +1,11 @@
+package machine
+
+//fun main() {
+//	println("Write how many cups of coffee you will need: ")
+//	val getFromUser = readln().toInt()
+//	println("For $getFromUser cups of coffee you will need:\n" +
+//			"${200 * getFromUser} ml of water\n" +
+//			"${50 * getFromUser} ml of milk\n" +
+//			"${15 * getFromUser} of coffee beans\n")
+//
+//}
